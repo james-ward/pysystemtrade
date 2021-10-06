ff77f81bfcaa39204a5f78542f1e9c2854482be7
James Ward <james@robomo.co>
2021-10-07 00:26:24 +0800

    Call strategy() directly

--- dashboard/app.py
+++ dashboard/app.py
@@ -91,4 +91,5 @@ def rolls():
 
 
 if __name__ == "__main__":
-    app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
+    strategy()
+    #app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
